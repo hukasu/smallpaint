457e91ee96ca23f4b37e4a1fd8e68e4a84b51485
Lucas Farias <lucasfnf@gmail.com>
2023-01-27 19:22:56 +0900

    Simplify intersection test and add bounding box impl

--- src/scene/obj/cylinder.rs
+++ src/scene/obj/cylinder.rs
@@ -1,9 +1,11 @@
 use crate::{
     common::Ray,
-    scene::obj::{SceneObjectGeometry, Plane}
+    scene::obj::{SceneObjectGeometry, Plane}, extension::vector_ext::OrthonormalVectorExt
 };
 
 use super::SELFINTERSECTION_TOLERANCE;
+
+use glm::GenNum;
 
 pub enum CylinderType {
     ThroughHole,
@@ -35,72 +37,39 @@ impl Cylinder {
 
     fn surface_intersection(&self, ray: &Ray) -> Option<(glm::DVec3, f64)> {
         let rdcd = glm::dot(*ray.direction(), *self.axis.direction());
-        if glm::is_approx_eq(&rdcd, &0.) {
-            // Skew lines
-            let n = glm::cross(*self.axis.direction(), *ray.direction());
-            let n2 = glm::cross(*ray.direction(), n);
-            let n1 = glm::cross(*self.axis.direction(), n);
-            // Point on the axis of the cylinder closest to ray
-            let cp = *self.axis.origin() + *self.axis.direction() * (glm::dot(*ray.origin() - *self.axis.origin(), n2)/(glm::dot(*self.axis.direction(), n2)));
-            if glm::distance(cp, *self.axis.origin()) < self.height / 2. {
-                // Point on the ray closest to axis of the cylinder
-                let rp = *ray.origin() + *ray.direction() * (glm::dot(*self.axis.origin() - *ray.origin(), n1)/(glm::dot(*ray.direction(), n1)));
-                let dist = glm::distance(cp, rp);
-                if dist < self.radius {
-                    let displ = (self.radius.powi(2) - dist.powi(2)).sqrt();
-                    [rp - ((*ray.direction() * displ)), rp + ((*ray.direction() * displ))].into_iter()
-                        .filter_map(
-                            |hp| {
-                                let t = glm::distance(hp, *ray.origin());
-                                if t > SELFINTERSECTION_TOLERANCE {
-                                    Some((glm::normalize(hp - cp), t))
-                                } else {
-                                    None
-                                }
-                            }
-                        )
-                        .min_by(|(_, a), (_, b)| a.total_cmp(b))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        } else {
-            let roro = glm::dot(*ray.origin(), *ray.origin());
-            let rord = glm::dot(*ray.origin(), *ray.direction());
-            let roco = glm::dot(*ray.origin(), *self.axis.origin());
-            let rocd = glm::dot(*ray.origin(), *self.axis.direction());
-            let rdco = glm::dot(*ray.direction(), *self.axis.origin());
-            let coco = glm::dot(*self.axis.origin(), *self.axis.origin());
-            let cocd = glm::dot(*self.axis.origin(), *self.axis.direction());
+        let roro = glm::dot(*ray.origin(), *ray.origin());
+        let rord = glm::dot(*ray.origin(), *ray.direction());
+        let roco = glm::dot(*ray.origin(), *self.axis.origin());
+        let rocd = glm::dot(*ray.origin(), *self.axis.direction());
+        let rdco = glm::dot(*ray.direction(), *self.axis.origin());
+        let coco = glm::dot(*self.axis.origin(), *self.axis.origin());
+        let cocd = glm::dot(*self.axis.origin(), *self.axis.direction());
 
-            let a = 1. - (rdcd).powi(2);
-            let b = 2. * (rord - rdco + rdcd * (cocd - rocd));
-            let c = roro - 2. * roco + coco - (cocd - rocd).powi(2) - self.radius.powi(2);
-            let delta = b.powi(2) - 4. * a * c;
-            if delta >= 0. {
-                [(-b + delta.sqrt()) / (2. * a), (-b - delta.sqrt()) / (2. * a)].into_iter()
-                    .filter_map(
-                        |t| {
-                            if t > SELFINTERSECTION_TOLERANCE {
-                                let m = rocd + t * rdcd - cocd;
-                                if m.abs() < self.height / 2. {
-                                    let axis_point = *self.axis.origin() + *self.axis.direction() * m;
-                                    let hit_point = *ray.origin() + *ray.direction() * t;
-                                    Some((glm::normalize(hit_point - axis_point), t))
-                                } else {
-                                    None
-                                }
+        let a = 1. - (rdcd).powi(2);
+        let b = 2. * (rord - rdco + rdcd * (cocd - rocd));
+        let c = roro - 2. * roco + coco - (cocd - rocd).powi(2) - self.radius.powi(2);
+        let delta = b.powi(2) - 4. * a * c;
+        if delta >= 0. {
+            [(-b + delta.sqrt()) / (2. * a), (-b - delta.sqrt()) / (2. * a)].into_iter()
+                .filter_map(
+                    |t| {
+                        if t > SELFINTERSECTION_TOLERANCE {
+                            let m = rocd + t * rdcd - cocd;
+                            if m.abs() < self.height / 2. {
+                                let axis_point = *self.axis.origin() + *self.axis.direction() * m;
+                                let hit_point = *ray.origin() + *ray.direction() * t;
+                                Some((glm::normalize(hit_point - axis_point), t))
                             } else {
                                 None
                             }
+                        } else {
+                            None
                         }
-                    )
-                    .min_by(|(_, a), (_, b)| a.total_cmp(b))
-            } else {
-                None
-            }
+                    }
+                )
+                .min_by(|(_, a), (_, b)| a.total_cmp(b))
+        } else {
+            None
         }
     }
 
@@ -142,6 +111,145 @@ impl SceneObjectGeometry for Cylinder {
     }
 
     fn bounding_box(&self) -> (glm::DVec3, glm::DVec3) {
-        todo!()
+        let (axis_orth_a, axis_orth_b) = self.axis.direction().orthonormal();
+        let (axis_orth_a, axis_orth_b) = (glm::normalize(axis_orth_a), glm::normalize(axis_orth_b));
+        let top = *self.axis.origin() + (*self.axis.direction() * (self.height / 2.)).map(|n| if n.is_nan() { 0. } else { n });
+        let bottom = *self.axis.origin() - (*self.axis.direction() * (self.height / 2.)).map(|n| if n.is_nan() { 0. } else { n });
+        [
+            top + (axis_orth_a * self.radius),
+            top - (axis_orth_a * self.radius),
+            top + (axis_orth_b * self.radius),
+            top - (axis_orth_b * self.radius),
+            bottom + (axis_orth_a * self.radius),
+            bottom - (axis_orth_a * self.radius),
+            bottom + (axis_orth_b * self.radius),
+            bottom - (axis_orth_b * self.radius),
+        ].into_iter()
+            .fold(
+                (glm::to_dvec3(std::f64::INFINITY), glm::to_dvec3(std::f64::NEG_INFINITY)),
+                |state, cur| {
+                    (
+                        glm::dvec3(
+                            cur.x.min(state.0.x),
+                            cur.y.min(state.0.y),
+                            cur.z.min(state.0.z)
+                        ),
+                        glm::dvec3(
+                            cur.x.max(state.1.x),
+                            cur.y.max(state.1.y),
+                            cur.z.max(state.1.z)
+                        )
+                    )
+                }
+            )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use glm::is_approx_eq;
+
+    #[test]
+    fn bounding_box_test() {
+        let cyl = Cylinder::new(
+            Ray::new(glm::to_dvec3(0.), glm::dvec3(1., 0., 0.)),
+            2.,
+            1.,
+            CylinderType::ThroughHole
+        );
+        let bb = cyl.bounding_box();
+        glm::assert_approx_eq!(bb.0, (glm::dvec3(-1., -1., -1.)));
+        glm::assert_approx_eq!(bb.1, (glm::dvec3(1., 1., 1.)));
+
+        let cyl = Cylinder::new(
+            Ray::new(glm::to_dvec3(0.), glm::dvec3(0., 1., 0.)),
+            2.,
+            1.,
+            CylinderType::ThroughHole
+        );
+        let bb = cyl.bounding_box();
+        glm::assert_approx_eq!(bb.0, (glm::dvec3(-1., -1., -1.)));
+        glm::assert_approx_eq!(bb.1, (glm::dvec3(1., 1., 1.)));
+
+        let cyl = Cylinder::new(
+            Ray::new(glm::to_dvec3(0.), glm::dvec3(0., 0., 1.)),
+            2.,
+            1.,
+            CylinderType::ThroughHole
+        );
+        let bb = cyl.bounding_box();
+        glm::assert_approx_eq!(bb.0, (glm::dvec3(-1., -1., -1.)));
+        glm::assert_approx_eq!(bb.1, (glm::dvec3(1., 1., 1.)));
+
+        let cyl = Cylinder::new(
+            Ray::new(glm::to_dvec3(0.), glm::dvec3(1., 0., 0.)),
+            200.,
+            1.,
+            CylinderType::ThroughHole
+        );
+        let bb = cyl.bounding_box();
+        glm::assert_approx_eq!(bb.0, (glm::dvec3(-100., -1., -1.)));
+        glm::assert_approx_eq!(bb.1, (glm::dvec3(100., 1., 1.)));
+
+        let cyl = Cylinder::new(
+            Ray::new(glm::to_dvec3(0.), glm::dvec3(1., 0., 0.)),
+            std::f64::INFINITY,
+            1.,
+            CylinderType::ThroughHole
+        );
+        let bb = cyl.bounding_box();
+        assert!(bb.0.x.is_infinite() && bb.0.x.is_sign_negative());
+        assert!(bb.1.x.is_infinite() && bb.1.x.is_sign_positive());
+        glm::assert_approx_eq!(bb.0.truncate(0), glm::to_dvec2(-1.));
+        glm::assert_approx_eq!(bb.1.truncate(0), glm::to_dvec2(1.));
+
+        let cyl = Cylinder::new(
+            Ray::new(glm::to_dvec3(0.), glm::normalize(glm::dvec3(1., 1., 0.))),
+            std::f64::INFINITY,
+            1.,
+            CylinderType::ThroughHole
+        );
+        let bb = cyl.bounding_box();
+        assert!(bb.0.x.is_infinite() && bb.0.x.is_sign_negative());
+        assert!(bb.1.x.is_infinite() && bb.1.x.is_sign_positive());
+        assert!(bb.0.y.is_infinite() && bb.0.y.is_sign_negative());
+        assert!(bb.1.y.is_infinite() && bb.1.y.is_sign_positive());
+        glm::assert_approx_eq!(bb.0.z, -1.);
+        glm::assert_approx_eq!(bb.1.z, 1.);
+
+        let cyl = Cylinder::new(
+            Ray::new(glm::to_dvec3(0.), glm::normalize(glm::dvec3(1., 1., 1.))),
+            std::f64::INFINITY,
+            1.,
+            CylinderType::ThroughHole
+        );
+        let bb = cyl.bounding_box();
+        assert!(bb.0.x.is_infinite() && bb.0.x.is_sign_negative());
+        assert!(bb.1.x.is_infinite() && bb.1.x.is_sign_positive());
+        assert!(bb.0.y.is_infinite() && bb.0.y.is_sign_negative());
+        assert!(bb.1.y.is_infinite() && bb.1.y.is_sign_positive());
+        assert!(bb.0.z.is_infinite() && bb.0.z.is_sign_negative());
+        assert!(bb.1.z.is_infinite() && bb.1.z.is_sign_positive());
+
+        let cyl = Cylinder::new(
+            Ray::new(glm::to_dvec3(0.), glm::normalize(glm::dvec3(1., 1., 0.))),
+            2.,
+            1.,
+            CylinderType::ThroughHole
+        );
+        let bb = cyl.bounding_box();
+        glm::assert_approx_eq!(bb.0, (glm::dvec3(-(2.0_f64).sqrt(), -(2.0_f64).sqrt(), -1.)));
+        glm::assert_approx_eq!(bb.1, (glm::dvec3((2.0_f64).sqrt(), (2.0_f64).sqrt(), 1.)));
+
+        let cyl = Cylinder::new(
+            Ray::new(glm::to_dvec3(0.), glm::normalize(glm::dvec3(1., 0., 1.))),
+            2.,
+            1.,
+            CylinderType::ThroughHole
+        );
+        let bb = cyl.bounding_box();
+        glm::assert_approx_eq!(bb.0, (glm::dvec3(-(2.0_f64).sqrt(), -1., -(2.0_f64).sqrt())));
+        glm::assert_approx_eq!(bb.1, (glm::dvec3((2.0_f64).sqrt(), 1., (2.0_f64).sqrt())));
     }
 }
